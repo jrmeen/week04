f74a8d659c9059c3336e4a2f033130f6d6c74778
meen <59030143@kmitl.ac.th>
2017-10-26 21:33:53 +0700

    w04p17bool

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,10 +5,9 @@ namespace variableProperties
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : char");
-            Console.WriteLine("Size :" + sizeof(char));
-            Console.WriteLine("Minimum Value :" + char.MinValue);
-            Console.WriteLine("Maximum Value :" + char.MaxValue);
+            Console.WriteLine("Data type : bool");
+            Console.WriteLine("Size :" + sizeof(bool));
+            
         }
     }
 }
