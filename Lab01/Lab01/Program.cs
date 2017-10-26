190b3ba167c9772d232bb2650b136367c433bd50
meen <59030143@kmitl.ac.th>
2017-10-26 21:35:54 +0700

    w04p18 sbyte

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,9 +5,10 @@ namespace variableProperties
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : bool");
-            Console.WriteLine("Size :" + sizeof(bool));
-            
+            Console.WriteLine("Data type : sbyte");
+            Console.WriteLine("Size :" + sizeof(sbyte));
+            Console.WriteLine("Minimum Value :" + int.MinValue);
+            Console.WriteLine("Maximum Value :" + int.MaxValue);
         }
     }
 }
