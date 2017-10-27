dce4c50d04b0da714348f9e09c6c1162cccfc397
meen <59030143@kmitl.ac.th>
2017-10-27 20:18:11 +0700

    w04p19 short

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,8 +5,8 @@ namespace variableProperties
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : sbyte");
-            Console.WriteLine("Size :" + sizeof(sbyte));
+            Console.WriteLine("Data type : short");
+            Console.WriteLine("Size :" + sizeof(short));
             Console.WriteLine("Minimum Value :" + int.MinValue);
             Console.WriteLine("Maximum Value :" + int.MaxValue);
         }
