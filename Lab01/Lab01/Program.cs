a209a6bfeabde1a59132da7e02b2abb2ec611d09
meen <59030143@kmitl.ac.th>
2017-10-27 21:18:02 +0700

    w04p36

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,17 +1,39 @@
-﻿using System; 
- public class intergerTest
- { 
-     static void Main(string[] args)
-     {          double a = 10, b = 20, x = 5, y = 2; 
-         Console.WriteLine("{0} + {1} = {2}",a,b,a+b); 
-         Console.WriteLine("{0} - {1} = {2}", x,b,x-b); 
-         Console.WriteLine("{0} * {1} = {2}",x,b,x* b); 
-         Console.WriteLine("{0} / {1} = {2}",y,a,y/a); 
-         Console.WriteLine("{0} % {1} = {2}",b,y,b%y); 
-         Console.WriteLine("{0} +10 % {1} = {2}",y,x,y+10%x); 
-         Console.WriteLine("{0} / 3 * 5 = {1}",a,a/3*5); 
-         Console.WriteLine("9 / 2 * {0} = {1}", a,9/2* a); 
-         Console.WriteLine("{0} % 8= {1}", y,y%8); 
-         Console.WriteLine("100 * {0} + {1} % 2 - {2} = {3}",x,y,a,100* x+y%2-a); 
-     } 
- } 
+﻿
+using System;
+
+namespace variableProperties
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            const double lightSpeed = 186000d;   // miles per second
+
+            Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
+
+            const double mileTokm = 1.609344;
+
+            Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
+
+            const double SunToEarthDistance = 93000000d;  // miles
+
+            Console.WriteLine("SunToEarthDistance = {0} km", SunToEarthDistance * mileTokm);
+
+            double SunToEarthTimeOfLight = SunToEarthDistance / lightSpeed;  // miles
+
+            Console.WriteLine("SunToEarthTimeOfLight = {0} seconds", SunToEarthTimeOfLight);
+
+            Console.WriteLine("SunToEarthTimeOfLight = {0} minutes", SunToEarthTimeOfLight / 60d);
+
+        }
+
+    }
+
+}
+
