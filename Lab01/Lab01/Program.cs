a479d8bae100dbdff4e9d4edf85369509e73951d
meen <59030143@kmitl.ac.th>
2017-10-27 20:48:09 +0700

    or

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,13 +10,13 @@ namespace thruthTable
             Console.WriteLine("-----------------------");
             Console.WriteLine("   A      B\t|  Y");
             Console.WriteLine("-----------------------");
-            A = false; B = false; Y = A & B;
+            A = false; B = false; Y = A | B;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = false; B = true; Y = A & B;
+            A = false; B = true; Y = A | B;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = false; Y = A & B;
+            A = true; B = false; Y = A | B;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = true; Y = A & B;
+            A = true; B = true; Y = A | B;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
             Console.WriteLine("-----------------------");
         }
