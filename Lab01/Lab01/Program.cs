a3c83292cf4b8cf0cd419db10c63b7136f5e7b4a
meen <59030143@kmitl.ac.th>
2017-10-27 20:29:04 +0700

    long

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,8 +5,8 @@ namespace variableProperties
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : decimal");
-            Console.WriteLine("Size :" + sizeof(decimal));
+            Console.WriteLine("Data type : long");
+            Console.WriteLine("Size :" + sizeof(long));
             Console.WriteLine("Minimum Value :" + int.MinValue);
             Console.WriteLine("Maximum Value :" + int.MaxValue);
         }
