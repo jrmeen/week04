f076e8a585ca68ac30cbea1dcc1347fb0c87513f
meen <59030143@kmitl.ac.th>
2017-10-27 20:19:47 +0700

    ushort

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,8 +5,8 @@ namespace variableProperties
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : short");
-            Console.WriteLine("Size :" + sizeof(short));
+            Console.WriteLine("Data type : ushort");
+            Console.WriteLine("Size :" + sizeof(ushort));
             Console.WriteLine("Minimum Value :" + int.MinValue);
             Console.WriteLine("Maximum Value :" + int.MaxValue);
         }
