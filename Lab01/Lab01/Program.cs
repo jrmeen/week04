a89dafb91552f350d6503703b2cd3310df8544ed
meen <59030143@kmitl.ac.th>
2017-10-27 20:25:14 +0700

    double

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,8 +5,8 @@ namespace variableProperties
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : float");
-            Console.WriteLine("Size :" + sizeof(float));
+            Console.WriteLine("Data type : double");
+            Console.WriteLine("Size :" + sizeof(double));
             Console.WriteLine("Minimum Value :" + int.MinValue);
             Console.WriteLine("Maximum Value :" + int.MaxValue);
         }
